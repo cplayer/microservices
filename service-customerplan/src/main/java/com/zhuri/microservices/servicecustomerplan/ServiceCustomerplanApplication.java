91498742f9c2e53e40b72a5df99ef0cebdb6003b
TangWeilun <tang_wlhs@163.com>
2017-11-06 20:14:19 +0800

    create customer plan

---
+++ service-customerplan/src/main/java/com/zhuri/microservices/servicecustomerplan/ServiceCustomerplanApplication.java
@@ -0,0 +1,14 @@
+package com.zhuri.microservices.servicecustomerplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceCustomerplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCustomerplanApplication.class, args);
+	}
+}
