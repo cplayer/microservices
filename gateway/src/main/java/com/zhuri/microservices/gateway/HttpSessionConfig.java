dc2c7f4ee03084fc15479d77ef2d7e287ec1f7b9
TangWeilun <tang_wlhs@163.com>
2017-11-06 19:26:32 +0800

    delete authorization

--- gateway/src/main/java/com/zhuri/microservices/gateway/HttpSessionConfig.java
+++
@@ -1,7 +0,0 @@
-package com.zhuri.microservices.gateway;
-
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@EnableRedisHttpSession
-public class HttpSessionConfig {
-}
