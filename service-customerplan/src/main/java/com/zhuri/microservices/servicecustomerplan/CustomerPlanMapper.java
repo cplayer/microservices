bccd6c7aa04977f1254ec522df907b22c4579d33
TangWeilun <tang_wlhs@163.com>
2017-12-09 21:14:54 +0800

    add update.

--- service-customerplan/src/main/java/com/zhuri/microservices/servicecustomerplan/CustomerPlanMapper.java
+++ service-customerplan/src/main/java/com/zhuri/microservices/servicecustomerplan/CustomerPlanMapper.java
@@ -13,10 +13,15 @@ public interface CustomerPlanMapper {
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     int addCustomerPlan(CustomerPlan customerPlan);
 
+    @Update("Update customerplan " +
+            " SET name=#{name}, customerId=#{customerId}, brandId=#{brandId}, saleDate=#{saleDate}, status=#{status}, createDate=#{createDate} " +
+            " WHERE id = #{id}")
+    void updateCustomerPlan(CustomerPlan customerPlan);
+
     @Select("SELECT * from customerplan WHERE customerId = #{customerId}")
     List<CustomerPlan> getCustomerPlansByCustomerId(int customerId);
 
-    //status = 0 草稿 1 提交审核 2 已审核 3 申请完成 4 确认完成
+    //status = 1 提交审核 2 已审核
     @Update("UPDATE customerplan SET status=#{status} WHERE id=#{id}")
     int setCustomerPlanStatus(@Param("status")int status, @Param("id")int id);
 
@@ -31,7 +36,15 @@ public interface CustomerPlanMapper {
             " VALUES(#{customerPlanId}, #{eventId}, #{startTime}, #{endTime}, #{sort})")
     int addCustomerPlanEvent(CustomerPlanEvent customerPlanEvent);
 
+    @Update("Update customerplan_event " +
+            " SET customerPlanId=#{customerPlanId}, eventId=#{eventId}, startTime=#{startTime}, endTime=#{endTime}, sort=#{sort} " +
+            " WHERE id = #{id}")
+    void updateCustomerPlanEvent(CustomerPlanEvent customerPlanEvent);
+
     @Select("SELECT * FROM customerplan_event WHERE customerPlanId = #{customerPlanId}")
     List<CustomerPlanEvent> getCustomerPlanEventByCustomerPlanId( int customerPlanId);
+
+    @Delete("DELETE FROM customerplan_event WHERE id =#{id}")
+    void delete(int id);
 
 }
