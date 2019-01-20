92e5e4dc24d9458f138296f7cc13238c51592e46
TangWeilun <tang_wlhs@163.com>
2019-01-20 21:57:54 +0800

    升级到spring boot 2

--- service-hello/src/main/java/com/zhuri/microservices/servicehello/HelloController.java
+++ service-hello/src/main/java/com/zhuri/microservices/servicehello/HelloController.java
@@ -1,23 +1,15 @@
 package com.zhuri.microservices.servicehello;
 
-import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
-    private int serverPort;
-    @Override
-    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
-        this.serverPort = event.getEmbeddedServletContainer().getPort();
-
-    }
+public class HelloController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String sayHello(@RequestParam String name){
-        return "Hello " + name + ", greeting from port: " + serverPort;
+        return "Hello " + name + ", greeting from port: " ;
     }
 }
